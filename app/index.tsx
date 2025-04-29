eef1a5dbadbc3fa62d14e60391e0b64b57aa9871
singhbanta <singhbanta84@gmail.com>
2025-04-29 16:33:35 +0530

    add: webview in expo

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: "https:second-brain-frontend-khaki.vercel.app" }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
